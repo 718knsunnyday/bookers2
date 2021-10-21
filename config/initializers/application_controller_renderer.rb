f93bf6957ef1c1cb4e8baffb88df3601eccc1be0
EC2 Default User <ec2-user@ip-172-31-85-81.ec2.internal>
2021-10-21 12:31:46 +0000

    image追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7c37e6c1786dd301e10f0403afe091d2ff309d5e6ed4eefbae69e4aee3ec4586ad3f95cdfe376d452213fe331c93a559dfd772ed2c5ec297e779e9d50bf350cf'
