3c9c488d330c91cc19125ca09b65049aa0dab18f
EC2 Default User <ec2-user@ip-172-31-85-81.ec2.internal>
2021-10-21 11:18:42 +0000

    user,book/edit追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,20 +6,33 @@ class BooksController < ApplicationController
     @books=Book.all
   end
 
+  def show
+    @user=current_user
+    @book=Book.new
+    @books=Book.find(params[:id])
+  end
+
   def create
     @book=Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
-  end
-
-  def show
+    redirect_to book_path(@book.id)
   end
 
   def edit
+    @book=Book.find(params[:id])
+  end
+
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path
   end
 
   def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,33 @@
 class UsersController < ApplicationController
+
+  def index
+    @user=current_user
+    @book=Book.new
+    @users=User.all
+  end
+
   def show
     @user=User.find(params[:id])
+    @book=Book.new
+    @books=Book.where(user_id: @user.id)
+  end
 
+  def create
+    @book=Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def edit
-    @user=User.find(params[])
+    @user=User.find(params[:id])
   end
 
   def update
   end
 
+  private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
 end
